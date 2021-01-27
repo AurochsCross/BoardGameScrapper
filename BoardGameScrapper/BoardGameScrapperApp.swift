0c9adbf10058e529a0d2d485412a32b567164ef4
Petras Malinauskas <petras.malinauskas@unity3d.com>
2021-01-27 11:19:51 +0200

    Initial Commit

---
+++ BoardGameScrapper/BoardGameScrapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoardGameScrapperApp.swift
+//  BoardGameScrapper
+//
+//  Created by Petras Malinauskas on 2021-01-27.
+//
+
+import SwiftUI
+
+@main
+struct BoardGameScrapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BoardGameScrapper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BoardGameScrapper
+//
+//  Created by Petras Malinauskas on 2021-01-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
